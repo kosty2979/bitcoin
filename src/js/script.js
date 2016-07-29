8b88a61532c859ce7c1140318f1f15e02c4a91b6
kosty2979 <kosty2979@gmail.com>
2016-07-29 16:52:36 +0300

    add pages: about, terms; add slider to homepage

--- src/js/script.js
+++ src/js/script.js
@@ -8,5 +8,20 @@ $($menuLink).on("click", function(){
 $($closeMenu).on("click", function(){
 	$($menuContent).hide()
 })
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
  })
+
+
+
+
+
+
+
