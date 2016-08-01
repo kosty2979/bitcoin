0ba11f65b505f93b4b0d19cb0a3d6458f365e577
kosty2979 <kosty2979@gmail.com>
2016-08-01 17:27:25 +0300

    add page contacrs and payment options

--- src/js/script.js
+++ src/js/script.js
@@ -8,20 +8,41 @@ $($menuLink).on("click", function(){
 $($closeMenu).on("click", function(){
 	$($menuContent).hide()
 })
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'mapbox.streets'
-		}).addTo(mymap);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+$('.accord').on('hidden.bs.collapse', toggleChevron);
+$('.accord').on('shown.bs.collapse', toggleChevron);
+
  })
 
+function toggleChevron(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find("i.accord__ind")
+        .toggleClass('fa-chevron-down fa-chevron-up');
+}
 
+var contactMap = function(){
+	
+	var mymap = L.map('mapid').setView( [53.61, 10.001356], 10 );
 
+	var greenIcon = L.icon({
+	    iconUrl: "../image/mapIcon.png",
+	   
+	    iconSize:     [22, 33], // size of the icon
+	    
+	});
 
+	L.marker([53.676012, 10.001356], {icon: greenIcon}).addTo(mymap);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'kosty79.10hib9ca',
+	    accessToken: 'pk.eyJ1Ijoia29zdHk3OSIsImEiOiJjaXJibXdiYm4wMDVhaWxsdzFvdG0yYWk0In0.xVVsKeKHcP72EYKT2jj6xQ'
+	}).addTo(mymap);
+}
 
 
 
